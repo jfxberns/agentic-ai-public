c13b3417ece73032d9ce66798e833a29ccc24083
John Berns <jfxberns@gmail.com>
2026-04-26 13:29:48 +0700

    fix: update TemplateResponse calls for Starlette compatibility

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class PromptRequest(BaseModel):
 
 @app.get("/", response_class=HTMLResponse)
 def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": Request}, headers={})
 
 
 @app.get("/api", response_class=JSONResponse)
